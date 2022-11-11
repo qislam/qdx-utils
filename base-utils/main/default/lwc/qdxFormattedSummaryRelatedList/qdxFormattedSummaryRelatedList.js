01dc62e1a9759cbf58e30bf7cff916e9a8bddfe0
Qamar Islam <qislam@outlook.com>
2022-11-11 09:38:45 -0500

    Configurable component complete

--- base-utils/main/default/lwc/qdxFormattedSummaryRelatedList/qdxFormattedSummaryRelatedList.js
+++ base-utils/main/default/lwc/qdxFormattedSummaryRelatedList/qdxFormattedSummaryRelatedList.js
@@ -4,30 +4,12 @@ import getData from '@salesforce/apex/QDX_FormattedSummaryController.getData'
 export default class QdxFormattedSummaryRelatedList extends LightningElement {
     @api recordId;
     @api cardTitle;
-    @api relatedSobject;
-    @api relatedLookup;
-    @api relatedFields;
-    @api summaryFormat;
-    queryString = '';
-    formattedSummaries = [
-        {
-            id: "1",
-            value: "This is value 1"
-        }, {
-            id: "2",
-            value: "This is value 2"
-        }, {
-            id: "3",
-            value: "This is value 3"
-        }
-    ];
+    @api propertyLabel;
+    formattedSummaries = [];
 
     connectedCallback() {
-        this.queryString = 'SELECT ' + this.relatedFields 
-            + ' FROM ' + this.relatedSobject 
-            + ' WHERE ' + this.relatedLookup + ' = \'' + this.recordId + '\'' 
-            + ' ORDER BY CreatedDate DESC';
-        getData({ recordId: this.recordId, queryString: this.queryString, summaryFormat: this.summaryFormat})
+        
+        getData({ recordId: this.recordId, propertyLabel: this.propertyLabel})
             .then(result => {
                 if (result) {
                     this.formattedSummaries = result;
